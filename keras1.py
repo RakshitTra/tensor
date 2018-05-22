717cb0b50b78804ced1560ad40786a60bacdfd54
Rakshit <34887766+RakshitTra@users.noreply.github.com>
2018-05-22 07:20:40 +0530

    Update keras1.py

--- keras1.py
+++ keras1.py
@@ -5,7 +5,10 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
-
+import tensorflow as tf
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+from keras import backend as K
+print(K.tensorflow_backend._get_available_gpus())
 batch_size = 128
 num_classes = 10
 epochs = 12
